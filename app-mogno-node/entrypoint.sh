526e7d5f5b6969c80d7f4682c00b49b19a38bdc1
Yagya Adhikari <yagyaadhikari02@gmail.com>
2024-07-12 08:44:30 +0545

    changes location

--- app-mogno-node/entrypoint.sh
+++ app-mogno-node/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Function to check if Docker network exists
-network_exists() {
-    docker network inspect "$1" > /dev/null 2>&1
-}
-
-# Check if the network exists
-if ! network_exists my-net; then
-    echo "Creating Docker network: my-net"
-    docker network create my-net
-fi
-
-# Start your application using the network
-node index.js --net=my-net
+# Start your application
+echo "Starting application..."
+node index.js
